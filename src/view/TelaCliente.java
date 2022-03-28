7c50de865996c79e2e1399f7345ec987a21fae0c
gabri <gabri@LAPTOP-6CRIQP9J>
2022-03-28 19:43:37 -0300

    classes interface

--- src/view/TelaCliente.java
+++ src/view/TelaCliente.java
@@ -1,5 +1,18 @@
 package view;
 
-public class TelaCliente {
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class TelaCliente implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+
+    }
 
 }
