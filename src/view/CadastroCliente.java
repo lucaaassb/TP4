6dce08d72cdaca2f16d5c51b6eb6f5febce922a0
gabri <gabri@LAPTOP-6CRIQP9J>
2022-03-31 17:41:10 -0300

    checkBox

--- src/view/CadastroCliente.java
+++ src/view/CadastroCliente.java
@@ -7,21 +7,23 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import javax.swing.JCheckBox;
 
 public class CadastroCliente implements ActionListener {
 	private static JFrame janela = new JFrame ("DogInn");
-	private static JLabel labelNome = new JLabel("Nome: ");
+	private static JLabel labelNome = new JLabel("Nome");
 	private static JTextField valorNome = new JTextField(200);
-	private static JLabel labelEmail = new JLabel("email: ");
+	private static JLabel labelEmail = new JLabel("E-mail");
 	private static JTextField valorEmail = new JTextField(200);
-	private static JLabel labelEnd = new JLabel("Endereco: ");
+	private static JLabel labelEnd = new JLabel("Endereco");
 	private static JTextField valorEnd = new JTextField(200);
-	private static JLabel labelSenha = new JLabel("Senha ");
+	private static JLabel labelSenha = new JLabel("Senha");
 	private static JTextField valorSenha = new JTextField(200);
-	private static JLabel labelTelefone = new JLabel("Telefone: ");
+	private static JLabel labelTelefone = new JLabel("Telefone");
 	private static JTextField valorDDD = new JTextField(3);
 	private static JTextField valorTelefone = new JTextField(10);
 	private static JButton botaoCadastrar = new JButton("Cadastrar");
+	private static JCheckBox termosAceite = new JCheckBox("Li e concordo com os termos");
 	
 	public CadastroCliente () {
 		labelNome.setBounds(30, 20, 150, 25);
@@ -36,6 +38,7 @@ public class CadastroCliente implements ActionListener {
 		botaoCadastrar.setBounds(245, 175, 115, 30);
 		labelEmail.setBounds(30, 80, 150, 25);
 		valorEmail.setBounds(180, 80, 180, 25);
+		termosAceite.setBounds(20, 175, 190, 25);
 		
 		janela.setLayout(null);
 		
@@ -51,6 +54,7 @@ public class CadastroCliente implements ActionListener {
 		janela.add(valorDDD);
 		janela.add(valorTelefone);
 		janela.add(botaoCadastrar);
+		janela.add(termosAceite);
 		
 		janela.setSize(400, 250);
 		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
