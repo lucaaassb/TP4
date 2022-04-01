b3e0e2662f24c3729497611460cc2d644ea9e990
gabri <gabri@LAPTOP-6CRIQP9J>
2022-03-31 22:45:00 -0300

    serviço com getter e setter

--- src/Servico.java
+++ src/Servico.java
@@ -1,25 +1,13 @@
 
 public class Servico {
 	private String tipoServico;
-	
-	Servico(int tipoServico) {
-		
-		switch(tipoServico) {
-		case 1:
-			this.tipoServico = "Hospedagem";
-			break;
-		case 2:
-			this.tipoServico = "Creche";
-			break;
-		case 3:
-			this.tipoServico = "Pet Sitter";
-			break;
-		default:
-			this.tipoServico = "Outro serviço";
-		}
-	}
 
 	public String getTipoServico() {
 		return tipoServico;
 	}
+
+	public void setTipoServico(String tipoServico) {
+		this.tipoServico = tipoServico;
+	}
+	
 }
